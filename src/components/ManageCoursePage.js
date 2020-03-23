c8343c309fb25c287c466a1ee14cdc10a28f421c
Mohammad Muhiuddin <mmuhiuddin@corp.contoursoftware.net>
2020-03-23 12:42:57 +0500

    Course form added

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = props => {
-  debugger;
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: null
+  });
+
+  function handleChange({ target }) {
+    setCourse({
+      ...course,
+      [target.name]: target.value
+    });
+  }
+
   return (
     <>
       <h2>Manage Course </h2>
-      {props.match.params.slug}
+      <CourseForm course={course} onChanged={handleChange} />
     </>
   );
 };
---
+++ src/components/common/TextInput.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function TextInput(props) {
+  return (
+    <div className="form-group">
+      <label htmlFor={props.id}>{props.label}</label>
+      <div className="field">
+        <input
+          id={props.id}
+          type="text"
+          name={props.name}
+          onChange={props.onChanged}
+          className="form-control"
+          value={props.value}
+        />
+      </div>
+      {props.error && <div className="alert alert-danger">{props.error}</div>}
+    </div>
+  );
+}
+
+export default TextInput;
